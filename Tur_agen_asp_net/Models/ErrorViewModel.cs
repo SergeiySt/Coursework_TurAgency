073d951c6e9249bf19bbac7726f16f4c9d1d0758
SergeiySt <ser.st.1989@gmail.com>
2023-10-10 15:06:57 +0300

    add_1

---
+++ Tur_agen_asp_net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tur_agen_asp_net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
