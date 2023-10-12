b4403f10d881faebc19497ff36e72e4824089eb1
SergeiySt <ser.st.1989@gmail.com>
2023-10-12 20:11:03 +0300

    add_10

---
+++ Tur_agen_asp_net/Models/ApDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tur_agen_asp_net.Models
+{
+    public class ApDbContext : DbContext
+    {
+        public ApDbContext(DbContextOptions<ApDbContext> options) : base(options) { }
+
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<TourOrder> TourOrders { get; set; }
+        public DbSet<Users> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TourOrder>()
+                .HasOne(to => to.Tour)
+                .WithMany()
+                .HasForeignKey(to => to.id_tour);
+
+            modelBuilder.Entity<TourOrder>()
+                .HasOne(to => to.User)
+                .WithMany()
+                .HasForeignKey(to => to.id_users);
+
+            modelBuilder.Entity<TourOrder>()
+                .Property(to => to.TOSumm)
+                .HasColumnType("decimal(18, 4)");
+
+            modelBuilder.Entity<Tour>()
+                .Property(to => to.TPrice)
+                .HasColumnType("decimal(18, 4)");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=192.168.226.130; Database=db_news;User=sa;Password=Colorado2023; TrustServerCertificate=True;");
+    }
+
+}
---
+++ Tur_agen_asp_net/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Tur_agen_asp_net.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Country_name { get; set; }
+        public string Picture { get; set; }
+    }
+
+    
+}
