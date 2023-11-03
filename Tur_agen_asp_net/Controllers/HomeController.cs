8fe08e87e60d95106b679a0bbe6e43bfa043b3fe
SergeiySt <ser.st.1989@gmail.com>
2023-11-03 19:59:57 +0200

    add_89

---
+++ Tur_agen_asp_net/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tur_agen_asp_net.Models;
+using System.Collections;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Tur_agen_asp_net.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+      
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        ApDbContext db = new ApDbContext();
+        public IActionResult Index()
+        {
+             ViewBag.ShowTourManagement = true;
+             return View(db.Tour.ToList());
+           // return View();
+        }
+
+    public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
